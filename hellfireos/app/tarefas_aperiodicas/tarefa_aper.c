07d2979f1de021cf1c77613d8accc1753200ab6d
renataubueno <renataubueno@gmail.com>
2019-04-25 16:17:31 -0300

    created the tarefas_aperiodicas app with the polling server task, the best-effort task and the real time task

---
+++ hellfireos/app/tarefas_aperiodicas/tarefa_aper.c
@@ -0,0 +1,48 @@
+//foi usado como base o sched_test2.c
+#include <hellfire.h>
+
+//tarefa Polling Server - a geração dessa tarefa segue o mesmo formato da de tempo real
+void task_polling_server(void){
+	int32_t jobs, id;
+
+	id = hf_selfid();
+	for(;;){
+		jobs = hf_jobs(id);
+		printf("\n%s (%d)[%d][%d]", hf_selfname(), id, hf_jobs(id), hf_dlm(id));
+		while (jobs == hf_jobs(id));
+	}
+}
+
+//tarefa de tempo real
+void task_tempo_real(void){
+	int32_t jobs, id;
+
+	id = hf_selfid();
+	for(;;){
+		jobs = hf_jobs(id);
+		printf("\n%s (%d)[%d][%d]", hf_selfname(), id, hf_jobs(id), hf_dlm(id));
+		while (jobs == hf_jobs(id));
+	}
+}
+
+//tarefa best-effort (disparo de uma tarefa aperiódica entre 50ms e 500ms)
+void task_best_effort(void){
+	int32_t jobs, id;
+
+	id = hf_selfid();
+	for(;;){
+		delay_ms(50 + (random() % 450));
+		hf_spawn(task_polling_server, 0, 2, 0, "task c", 2048);
+		//trigga o spawn da tarefa aperiódica depois de um random entre 50 e 500
+		//tirando isso, o restante do funcionamento da tarefa é igual ao de tempo real
+		jobs = hf_jobs(id);
+		printf("\n%s (%d)[%d][%d]", hf_selfname(), id, hf_jobs(id), hf_dlm(id));
+		while (jobs == hf_jobs(id));
+	}
+}
+
+void app_main(void){
+	hf_spawn(task_tempo_real, 4, 1, 4, "tarefa de tempo real", 2048);
+	hf_spawn(task_best_effort, 0, 0, 0, "tarefa de best effort", 2048); 
+	//conforme a definição do hf_spawn(), If a task has defined realtime parameters, it is put on the RT queue, if not (period 0, capacity 0 and deadline 0), it is put on the BE queue.
+}
